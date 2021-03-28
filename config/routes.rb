e79a9e0b16b435346cb0b852340b33701e75f69c
Ryan Slater <ryan.slater157@gmail.com>
2021-03-28 17:19:22 +0100

    another attempt to revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  resources :tutors
-  resources :children
-  resources :adults
-  devise_for :users, controllers: {
-  	sessions: 'users/sessions'
-  }
-  resources :addresses
   root "home#home"
 
   get "/home", to: "home#home"
-  # Sign up might require a call to hte adult controller.
 end
