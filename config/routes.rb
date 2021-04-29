ffc0dee8cb7f68b6616bdbf70180373f8c5ac84d
Tados99 <csanakia@gmail.com>
2021-04-29 08:55:47 +0100

    LearningCenter is established

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
 
   devise_for :users, :path => 'u'
   resources :users, only: [:index, :show, :new, :edit, :update, :create]
-  #resources :users do 
+  #resources :users do
     #resources :recruiters #-> url.com/users/:user_id/recruiters
   #end
 
   get 'pages/about_us'
   get 'pages/contact'
+  get 'pages/hub'
   post 'request_contact', to: 'pages#request_contact'
   get 'pages/tutor_list'
   get 'pages/bootstrap_test'
