499189036ed62b1002e9029bbb8e2dc34d364505
celis1080 <cellis1080@gmail.com>
2021-05-16 15:31:15 +0100

    Fixed a params bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!, only: [:show]
 
 	def show
-	  	@user = current_user
+	  	@user = User.find(params[:id])
 	end
 
 	def new
